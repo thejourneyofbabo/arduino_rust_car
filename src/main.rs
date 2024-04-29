e0be1607bf5dd4dd2a56a1e51c3bfb4abbc189f8
Jisang Yun <journeyofbabo@gmail.com>
2024-04-30 05:09:09 +0900

    servo add

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,31 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    // Important because this sets the bit in the DDR register!
+    pins.d9.into_output();
 
-    let mut led = pins.d13.into_output();
+    // - TC1 runs off a 250kHz clock, with 5000 counts per overflow => 50 Hz signal.
+    // - Each count increases the duty-cycle by 4us.
+    // - Use OC1A which is connected to D9 of the Arduino Uno.
+    let tc1 = dp.TC1;
+    tc1.icr1.write(|w| w.bits(4999));
+    tc1.tccr1a
+        .write(|w| w.wgm1().bits(0b10).com1a().match_clear());
+    tc1.tccr1b
+        .write(|w| w.wgm1().bits(0b11).cs1().prescale_64());
 
     loop {
-        led.toggle();
+        // Move servo to front position (400 counts => ~1.6ms)
+        tc1.ocr1a.write(|w| w.bits(400));
+        arduino_hal::delay_ms(1000);
+
+        // Move servo to right position (700 counts => ~2.8ms)
+        tc1.ocr1a.write(|w| w.bits(700));
+        arduino_hal::delay_ms(1000);
+
+        // Move servo to left position (100 counts => ~0.4ms)
+        tc1.ocr1a.write(|w| w.bits(100));
         arduino_hal::delay_ms(1000);
     }
 }
+
